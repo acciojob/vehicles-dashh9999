f54184f72122a4dd5232a4289326dc1401d75352
prasadpatil079 <prasadpatil079@gmail.com>
2023-10-02 18:04:48 +0530

    first

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,18 @@
+package com.driver;
+
+public class Boat implements WaterVehicle{
+
+    public Boat() {
+    }
+
+    @Override
+    public String getVehicleName() {
+        return this.getVehicleName();
+    }
+
+    @Override
+    public int getVehicleCapacity() {
+
+        return this.getVehicleCapacity();
+    }
+}
